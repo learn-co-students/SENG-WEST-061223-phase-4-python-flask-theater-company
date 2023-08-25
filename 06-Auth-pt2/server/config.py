1dabb2e5f98e515a4f9897822a81e0a639a71e7d
betalantz <betalantz@gmail.com>
2023-08-25 15:38:17 -0700

    implement login

---
+++ 06-Auth-pt2/server/config.py
@@ -0,0 +1,23 @@
+# 1.✅ Import Bcrypt form flask_bcrypt
+# 1.1 Invoke Bcrypt and pass it app
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.json.compact = False
+
+app.secret_key = b"@~xH\xf2\x10k\x07hp\x85\xa6N\xde\xd4\xcd"
+
+db = SQLAlchemy()
+migrate = Migrate(app, db)
+
+bcrypt = Bcrypt(app)
+
+db.init_app(app)
+
+api = Api(app)
