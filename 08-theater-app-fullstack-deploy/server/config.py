02cf41432c512c154a1d6f25d58bafb30f45dfa4
betalantz <betalantz@gmail.com>
2023-08-30 11:03:55 -0700

    remove old migration/merge new

--- 08-theater-app-fullstack-deploy/server/config.py
+++ 08-theater-app-fullstack-deploy/server/config.py
@@ -1,17 +1,22 @@
 # 1.✅ Import Bcrypt form flask_bcrypt
 # 1.1 Invoke Bcrypt and pass it app
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 
+load_dotenv()
+
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
-app.secret_key = b"@~xH\xf2\x10k\x07hp\x85\xa6N\xde\xd4\xcd"
+app.secret_key = os.environ.get("SECRET_KEY")
 
 db = SQLAlchemy()
 migrate = Migrate(app, db)
